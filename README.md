1b2ff948744c14e4e755664ae766265f0c23b6d1
Bruno Bossola <bbossola@gmail.com>
2020-06-12 13:14:36 +0100

    First tool: license.py

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
-# api-samples-python
-A set of sample calls to the Meterian API in Python
+# Python sample API scripts
+A set of sample calls to the Meterian API in Python: use simple python scripts to leverage the power of the [Meterian API](http://api.meterian.io)
+
+## You will need a token to use these tools!
+
+All these tool will require an API token from Meterian. This is available for any paid plan, and it can be generated at  https://meterian.com/account/#tokens
+
+Once you have the token, the best and secure way to used it is to put into an environment variable, called METERIAN_API_TOKEN. In linux, for example, you can simply do something like this:
+
+    export METERIAN_API_TOKEN=a902874d-50f2-464f-8707-780cd5f669a3
+(no, this is not a real token eheh!)
+
+In most of the commands however you can simply specify the token on the command llie using something like this:
+
+    ./yadda.py --token=a902874d-50f2-464f-8707-780cd5f669a3
+
+# Tools
+
+## license&#46;py
+This script allows you to list the licenses of a component across [the platforms managed by Meterian](https://www.meterian.com/integrations.html#languages) . Simply specify the language, the full name of the library and the version: the tool will let you know the licenses found. Sometime it's instant, sometimes it may take a couple of seconds: do not worry, a result will eventually come :)
+
+Asking for **java** library, **io.vertx:vertx-core**, version **3.9.1**
+
+    $ ./license.py java io.vertx:vertx-core 3.9.1
+        Looking for license information about "io.vertx:vertx-core" version "3.9.1" in the "java" space...
+        Found 2 license(s):
+        - id:   Apache-2.0
+          name: Apache License 2.0
+          uri:  https://spdx.org/licenses/Apache-2.0.html
+        - id:   EPL-2.0
+          name: Eclipse Public License 2.0
+          uri:  https://spdx.org/licenses/EPL-2.0.html
+
+Asking for **nodejs** library, **less**, version **3.11.3**
+
+    $ ./license.py nodejs less 3.11.3
+    Looking for license information about "less" version "3.11.3" in the "nodejs" space...
+    Found 1 license(s):
+    - id:   Apache-2.0
+      name: Apache License 2.0
+      uri:  https://spdx.org/licenses/Apache-2.0.html
+
+And if you do not know what to do, ask for help!
+
+    $ ./license.py --help
+    usage: license.py [-h] [-t API-TOKEN] [-l LOGLEVEL] language name version
+
+    positional arguments:
+      language              The language of the library (i.e. java) ['java',
+                            'javascript', 'nodejs', 'python', 'dotnet', 'ruby',
+                            'scala', 'php', 'swift', 'golang']
+      name                  The full name of the library (i.e.
+                            com.fasterxml.jackson.core:jackson-databind)
+      version               The version of the library (i.e. 2.8.8)
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -t API-TOKEN, --token API-TOKEN
+                            Allows you to specify the API token to use directly on
+                            the command line. You can create your token with a
+                            bootstrap+ plan at
+                            https://meterian.com/account/#tokens
+      -l LOGLEVEL, --log LOGLEVEL
+                            Sets the logging level (default is warning)
+
+# Other general information
+## Common parameters
+As a common beheaviour across all the tools, it's also possible to specify the log level with any tool using  soething like this:
+
+    ./yadda.py --log=DEBUG
+Beware: the DEBUG level can be very verbose!
+
+
+## What's more?
+Well. you can build your onw tool using the Meterian API at https://api.meterian.io and you can also contribute to this repository!
---
+++ license.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python2.7
+
+import argparse
+from collections import namedtuple
+import logging
+import os
+import requests
+import sys
+import httplib
+import json
+
+API_TOKEN_ENVVAR = 'METERIAN_API_TOKEN'
+
+TIMEOUT = namedtuple('literal', 'text status_code')(text='{"status":"timeout"}', status_code=999)
+
+LANGUAGES = [ 'java', 'javascript', 'nodejs', 'python', 'dotnet', 'ruby', 'scala', 'php', 'swift', 'golang']
+
+
+class HelpingParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.stderr.write('\n')
+        sys.exit(-1)
+    
+
+def _logHttpRequests():
+    httplib.HTTPConnection.debuglevel = 1
+    
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+    logging.debug('Full debug log for HTTP requests enabled')
+
+def _parseArgs():
+    parser = HelpingParser()
+    
+    parser.add_argument('language', help='The language of the library\n (i.e. java) %s' % LANGUAGES);
+    parser.add_argument('name', help='The full name of the library\n (i.e. com.fasterxml.jackson.core:jackson-databind)');
+    parser.add_argument('version', help='The version of the library\n (i.e. 2.8.8)');
+ 
+    parser.add_argument(
+        '-t', 
+        '--token', 
+        metavar='API-TOKEN',
+        default=os.environ[API_TOKEN_ENVVAR],
+        help=(
+            'Allows you to specify the API token to use directly on the command line. '
+            'You can create your token with a bootstrap+ plan at https://meterian.com/account/#tokens'
+        )
+    )
+    
+    parser.add_argument(
+        '-l', 
+        '--log', 
+        default='warning',
+        metavar='LOGLEVEL',
+        help='Sets the logging level (default is warning)'
+    )
+    
+    return parser.parse_args()
+
+
+def _initLogging(args):
+    levels = {
+        'critical': logging.CRITICAL,
+        'error': logging.ERROR,
+        'warn': logging.WARNING,
+        'warning': logging.WARNING,
+        'info': logging.INFO,
+        'debug': logging.DEBUG
+    }
+    level = levels.get(args.log.lower())
+    if level is None:
+        raise ValueError('Invalid log level requested - must be in '+levels.keys());
+    
+    logging.basicConfig(level=level)
+    logging.basicConfig(format='%(time)s-%(levelname)s-%(message)s')
+
+    if level == logging.DEBUG:
+        _logHttpRequests()
+    else:
+        logging.getLogger('requests').setLevel(logging.WARNING)
+
+    logging.debug('Logging initiated')
+
+
+def _loadLicenses(args):
+
+    url = 'https://www.meterian.com/api/v1/licenses/library/%s/%s/%s' % (args.language, args.name, args.version)
+    logging.debug('Loading license info from url [%s]...', url)
+    
+    try:
+        result = requests.get(url, timeout=30, headers={'Authorization':'Token %s' % args.token})
+        logging.info('Result %s' % result.text )
+    except:
+        result = TIMEOUT
+ 
+    return json.loads(result.text)
+
+
+
+#
+# CLI entry point
+#
+
+if __name__ == '__main__':
+
+    args = _parseArgs()
+    _initLogging(args)
+        
+    if not args.language in LANGUAGES:
+        sys.stderr.write('invalid language: %s\n' % args.language);
+        sys.stderr.write('available languages: %s\n' % str(LANGUAGES));
+        sys.stderr.write('\n')
+        sys.exit(-1)
+
+    token = args.token
+    if token == None:
+        sys.stderr.write('No API token found.\nPlease specify it using the "%s" environment variable or use --token on the command line' % API_TOKEN_ENVVAR);
+        sys.stderr.write('\n')
+        sys.exit(-1)
+        
+    print 'Looking for license information about "%s" version "%s" in the "%s" space...' % (args.name, args.version, args.language)
+    licenses = _loadLicenses(args)
+    
+    print 'Found %d license(s):' % len(licenses)
+    for licenz in licenses:
+        print '- id:   ' + licenz["id"]
+        print '  name: ' + licenz["name"]
+        print '  uri:  ' + licenz["uri"]
+        
+
