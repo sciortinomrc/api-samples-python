6e74a659c960ed2e195f7954a55f6d257c09e47c
Bruno Bossola <bbossola@gmail.com>
2020-06-12 18:06:10 +0100

    Now the error message is displayed when the environment variable METERIAN_API_TOKEN is not set

--- license.py
+++ license.py
@@ -22,11 +22,11 @@ class HelpingParser(argparse.ArgumentParser):
         self.print_help()
         sys.stderr.write('\n')
         sys.exit(-1)
-    
+
 
 def _logHttpRequests():
     httplib.HTTPConnection.debuglevel = 1
-    
+
     requests_log = logging.getLogger("requests.packages.urllib3")
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
@@ -35,30 +35,31 @@ def _logHttpRequests():
 
 def _parseArgs():
     parser = HelpingParser()
-    
+
     parser.add_argument('language', help='The language of the library\n (i.e. java) %s' % LANGUAGES);
     parser.add_argument('name', help='The full name of the library\n (i.e. com.fasterxml.jackson.core:jackson-databind)');
     parser.add_argument('version', help='The version of the library\n (i.e. 2.8.8)');
- 
+
+    token = os.environ[API_TOKEN_ENVVAR] if API_TOKEN_ENVVAR in os.environ else None
     parser.add_argument(
-        '-t', 
-        '--token', 
+        '-t',
+        '--token',
         metavar='API-TOKEN',
-        default=os.environ[API_TOKEN_ENVVAR],
+        default=token,
         help=(
             'Allows you to specify the API token to use directly on the command line. '
             'You can create your token with a bootstrap+ plan at https://meterian.com/account/#tokens'
         )
     )
-    
+
     parser.add_argument(
-        '-l', 
-        '--log', 
+        '-l',
+        '--log',
         default='warning',
         metavar='LOGLEVEL',
         help='Sets the logging level (default is warning)'
     )
-    
+
     return parser.parse_args()
 
 
@@ -74,7 +75,7 @@ def _initLogging(args):
     level = levels.get(args.log.lower())
     if level is None:
         raise ValueError('Invalid log level requested - must be in '+levels.keys());
-    
+
     logging.basicConfig(level=level)
     logging.basicConfig(format='%(time)s-%(levelname)s-%(message)s')
 
@@ -90,13 +91,13 @@ def _loadLicenses(args):
 
     url = 'https://www.meterian.com/api/v1/licenses/library/%s/%s/%s' % (args.language, args.name, args.version)
     logging.debug('Loading license info from url [%s]...', url)
-    
+
     try:
         result = requests.get(url, timeout=30, headers={'Authorization':'Token %s' % args.token})
         logging.info('Result %s' % result.text )
     except:
         result = TIMEOUT
- 
+
     if result.status_code != 200:
         print 'Unable to succesfully contact the meterian server: %s' % str(result)
         return None
@@ -113,7 +114,7 @@ if __name__ == '__main__':
 
     args = _parseArgs()
     _initLogging(args)
-        
+
     if not args.language in LANGUAGES:
         sys.stderr.write('invalid language: %s\n' % args.language);
         sys.stderr.write('available languages: %s\n' % str(LANGUAGES));
@@ -125,15 +126,13 @@ if __name__ == '__main__':
         sys.stderr.write('No API token found.\nPlease specify it using the "%s" environment variable or use --token on the command line' % API_TOKEN_ENVVAR);
         sys.stderr.write('\n')
         sys.exit(-1)
-        
+
     print 'Looking for license information about "%s" version "%s" in the "%s" space...' % (args.name, args.version, args.language)
     licenses = _loadLicenses(args)
-    
+
     if licenses != None:
         print 'Found %d license(s):' % len(licenses)
         for licenz in licenses:
             print '- id:   ' + licenz["id"]
             print '  name: ' + licenz["name"]
             print '  uri:  ' + licenz["uri"]
-        
-
